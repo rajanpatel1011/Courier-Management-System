5cdca6e3b3b4395e2c329da195a7151408ac8110
Rajan <rajanpatel1011@gmail.com>
2026-05-16 11:12:21 -0400

    Add .gitignore file and import RequestParam in HomeController

--- src/main/java/com/courier/management/controller/HomeController.java
+++ src/main/java/com/courier/management/controller/HomeController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
