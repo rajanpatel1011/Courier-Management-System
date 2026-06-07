6c3331779b1430ae5146eb0c523628a6a7d30bfe
rajan <rajanpatel1011@gmail.com>
2026-06-07 13:26:08 -0400

    fix db connection

--- src/main/java/com/courier/management/CourierManagementApplication.java
+++ src/main/java/com/courier/management/CourierManagementApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.util.Map;
+
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
 public class CourierManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourierManagementApplication.class, args);
+        SpringApplication app = new SpringApplication(CourierManagementApplication.class);
+        app.setDefaultProperties(Map.of("spring.profiles.active", "dev"));
+        app.run(args);
     }
 }
